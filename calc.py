d26a4d37b92f5e43928822b6d04f06c95163c78f
Eugene <g.r.e.e.d@yandex.ru>
2019-09-24 15:04:34 +0300

    PEP8 calc with Trabis

--- calc.py
+++ calc.py
@@ -1,19 +1,19 @@
 import __future__
+from typing import List
+
 while True:
     num_1 = input()
     operator = input()
     num_2 = input()
     try:
-        lst_of_operators = ['/','+','-','*','**','//','%']
+        lst_of_operators: List[str] = ['/', '+', '-', '*', '**', '//', '%']
         if operator in lst_of_operators:
-            print(eval(compile(num_1 + ' ' + operator + ' ' + num_2, "<string>",'eval', __future__.division.compiler_flag)))
-    # eval() без compile() выдавал ответы при делении в формате int
+            print(eval(
+                compile(num_1 + ' ' + operator + ' ' + num_2, "<string>", 'eval', __future__.division.compiler_flag)))
+        # eval() без compile() выдавал ответы при делении в формате int
         else:
             print('Incorrect operator')
     except ZeroDivisionError:
         print('Division by Zero')
     except ValueError:
         print('Need a number')
-
-
-
