101d0a4765b4f5a6fb4da8803f28fe012ea97af1
Eugene <g.r.e.e.d@yandex.ru>
2019-10-29 21:40:57 +0300

    HW04

---
+++ HW_4/ex1.py
@@ -0,0 +1,13 @@
+def checkio(string1, string2):
+    word_in_both_string = []
+    string1 = string1.split(',')
+    string2 = string2.split(',')
+
+    for word in string1:
+        if word in string2:
+            word_in_both_string.append(word)
+    return ','.join(sorted(word_in_both_string))
+
+#print(checkio("hello,world", "hello,earth") == "hello")
+#print(checkio("one,two,three", "four,five,six") == "")
+#print(checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two")
---
+++ HW_4/ex3.py
@@ -0,0 +1,30 @@
+def create_intervals(set_data):
+    if len(set_data) == 0 or not all(isinstance(x, int) for x in set_data):
+        return None  # return None if incorrect input
+
+    first_cycle = True
+    nums = []
+    result = []
+
+    for num in set_data:
+
+        if first_cycle:
+            last_num = num  # remember last el for next cycles
+            first_cycle = False
+            nums.append(num)
+            continue
+
+        if last_num == num - 1: # if it's next num, add it to nums
+            nums.append(num)
+            last_num = num
+        else:                   # if it's not next num add interval to result
+            result.append((min(nums),max(nums)))
+            last_num = num
+            nums = [num]
+
+    result.append((min(nums), max(nums))) # add last interval
+    return result
+
+#print(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)])
+#print(create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)])
+#print(create_intervals({1,'2'}) != [(1,2)])
